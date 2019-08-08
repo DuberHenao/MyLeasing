bbf967abe87f9fe65ed9d28f6bf8c1f82888bbcd
Duber Andres Henao Vasquez <duberhenao@hotmail.com>
2019-08-08 12:57:47 -0500

    Complete DB

---
+++ MyLeasing.Web/Data/Entities/PropertyImage.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MyLeasing.Web.Data.Entities
+{
+    public class PropertyImage
+    {
+        public int Id { get; set; }
+
+        [Display(Name = "Image")]
+        [Required(ErrorMessage = "The field {0} is mandatory.")]
+        public string ImageUrl { get; set; }
+
+       // public Property Property { get; set; }  propiedad que se agregará después.
+
+        // TODO: Change the path when publish
+        public string ImageFullPath => $"https://TBD.azurewebsites.net{ImageUrl.Substring(1)}";
+
+        public Property Property { get; set; }
+
+    }
+}
