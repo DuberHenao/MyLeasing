3bd97bf19008b2091c099b8cd23a191f0aafaf74
Duber Andres Henao Vasquez <duberhenao@hotmail.com>
2019-08-16 23:04:15 -0500

    Add user entities

---
+++ MyLeasing.Web/Data/Entities/Manager.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using MyLeasing.Web.Data.Entities;
+
+namespace MyLeasing.Web.Data.Entities
+{
+    public class Manager
+    {
+        public int Id { get; set; }
+
+        public User User { get; set; }
+    }
+}
+
