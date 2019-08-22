56bb424d07113c17699a5456fa9b62c675180f4b
Duber Andres Henao Vasquez <duberhenao@hotmail.com>
2019-08-22 11:24:21 -0500

    Edit Owner

--- MyLeasing.Web/Models/AddUserViewModel.cs
+++ MyLeasing.Web/Models/AddUserViewModel.cs
@@ -2,36 +2,16 @@
 
 namespace MyLeasing.Web.Models
 {
-    public class AddUserViewModel
+    public class AddUserViewModel : EditUserViewModel
     {
+        //Esto no esta en la clase heredada, en el video el profesor lo borró.
         [Display(Name = "Email")]
         [Required(ErrorMessage = "The field {0} is mandatory.")]
         [MaxLength(100, ErrorMessage = "The {0} field can not have more than {1} characters.")]
         [EmailAddress]
         public string Username { get; set; }
 
-        [Display(Name = "Document")]
-        [MaxLength(20, ErrorMessage = "The {0} field can not have more than {1} characters.")]
-        [Required(ErrorMessage = "The field {0} is mandatory.")]
-        public string Document { get; set; }
-
-        [Display(Name = "First Name")]
-        [MaxLength(50, ErrorMessage = "The {0} field can not have more than {1} characters.")]
-        [Required(ErrorMessage = "The field {0} is mandatory.")]
-        public string FirstName { get; set; }
-
-        [Display(Name = "Last Name")]
-        [MaxLength(50, ErrorMessage = "The {0} field can not have more than {1} characters.")]
-        [Required(ErrorMessage = "The field {0} is mandatory.")]
-        public string LastName { get; set; }
-
-        [MaxLength(100, ErrorMessage = "The {0} field can not have more than {1} characters.")]
-        public string Address { get; set; }
-
-        [Display(Name = "Phone Number")]
-        [MaxLength(50, ErrorMessage = "The {0} field can not have more than {1} characters.")]
-        public string PhoneNumber { get; set; }
-
+        
         [Display(Name = "Password")]
         [Required(ErrorMessage = "The field {0} is mandatory.")]
         [DataType(DataType.Password)]
